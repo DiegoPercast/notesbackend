da91423b5616e52446cb8c37a469b6a1adaa43dd
DiegoPercast <nmadara87@gmail.com>
2023-11-19 14:32:59 -0700

    refactored the code to its respective folder so it is easier to understand

--- index.js
+++ index.js
@@ -1,102 +1,10 @@
-require("dotenv").config();
-const Note = require("./models/note");
-const express = require("express");
-const cors = require("cors");
-const app = express();
+const app = require('./app')
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-app.use(cors());
-app.use(express.static("build"));
-app.use(express.json());
+const server = http.createServer(app)
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
-});
-
-app.get("/api/notes", (req, res) => {
-  Note.find({}).then((notes) => {
-    res.json(notes);
-  });
-});
-
-app.get("/api/notes/:id", (req, res, next) => {
-  Note.findById(req.params.id).then(note => {
-    if (note) {
-      res.json(note)
-    } else {
-      response.status(404).end()
-    }
-  })
-  .catch(error => next(error))
-});
-
-app.delete("/api/notes/:id", (req, res, next) => {
-  Note.findByIdAndDelete(req.params.id).then(deletedNote => {
-    res.status(204).end()
-    console.log(deletedNote)
-  })
-  .catch(error => next(error))
-});
-
-app.put("/api/notes/:id", (req, res, next) => {
-  const body = req.body
-
-  const note = {
-    content: body.content,
-    important: body.important 
-  }
-
-  Note.findByIdAndUpdate(req.params.id, note, { new: true })
-    .then(updatedNote => {
-      res.json(updatedNote)
-    })
-    .catch(error => next(error))
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
-
-app.post("/api/notes", (req, res, next) => {
-  const body = req.body;
-
-  if (body.content === undefined) {
-    return res.status(400).json({
-      error: "content-missing",
-    });
-  }
-
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-  });
-
-  note.save()
-    .then(savedNote => savedNote.toJSON())
-    .then(savedAndFormattedNote => {
-      res.json(savedAndFormattedNote)
-    })
-    .catch(error => next(error))
-});
-
-const unknownEndpoint = (req, res) => {
-  res.status(404).json({ error: 'unknown endpoint' })
-}
-
-app.use(unknownEndpoint)
-
-const errorHandler = (error, req, res, next) => {
-  console.error(error.message)
-
-  if (error.name === 'CastError') {
-    return res.status(400).json({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return res.status(400).json({ error: error.message })
-  }
-
-  next(error)
-}
-
-app.use(errorHandler)
-
-
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
--- models/note.js
+++ models/note.js
@@ -1,36 +1,24 @@
 const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI
+const noteSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  important: Boolean,
+})
 
-console.log('Connecting to', url)
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
-mongoose.connect(url)
-  .then(result => {
-    console.log('Connected to MongoDB')
-  })
-  .catch(error => {
-    console.log(`error connecting to MongoDB: ${error.message}`)
-  })
-
-  const noteSchema = new mongoose.Schema({
-    content: {
-      type: String,
-      minlength: 5,
-      required: true
-    },
-    date: {
-      type: Date,
-      required: true
-    },
-    important: Boolean
-  })
-
-  noteSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
-
-  module.exports = mongoose.model('Note', noteSchema)
+module.exports = mongoose.model('Note', noteSchema)
